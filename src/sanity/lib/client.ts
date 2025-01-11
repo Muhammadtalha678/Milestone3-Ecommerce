7f6d36cdabcd596bc236cfb81c9a7e34800e9bb3
Muhammadtalha678 <muhammadtalha0110@gmail.com>
2025-01-11 10:22:59 +0500

    edit env variables in client.ts

--- src/sanity/lib/client.ts
+++ src/sanity/lib/client.ts
@@ -1,11 +1,11 @@
 import { createClient } from 'next-sanity'
 
-import { apiVersion, dataset, projectId } from '../env'
+import { apiVersion, dataset, projectId,token } from '../env'
 
 export const client = createClient({
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
-  token: process.env.SANITY_API_TOKEN,
-  apiVersion:'2025-01-06',
+  projectId,
+  dataset,
+  token,
+  apiVersion,
   useCdn: false, // Set to false if statically generating pages, using ISR or tag-based revalidation
 })
