aee1cb8b7a44f19e57906b1969b3325ac8ce8b27
Muhammadtalha678 <muhammadtalha0110@gmail.com>
2025-01-15 02:23:43 +0500

    update api route n action

--- src/actions/checkOut.action.ts
+++ src/actions/checkOut.action.ts
@@ -7,7 +7,7 @@ export const checkOutUser = async(formData:FormData) => {
     const city = formData.get('city')
     const country = formData.get('country')
     try {
-        const response = await fetch("/api/user",
+        const response = await fetch("https://milestone3-ecommerce-rho.vercel.app/api/user",
             {
                 method: 'POST',
                 headers: {
