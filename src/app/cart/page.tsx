924ddd5be370726880d701f76cf23fba69cdef48
Muhammadtalha678 <muhammadtalha0110@gmail.com>
2025-01-15 10:32:49 +0500

    add checkout loader

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -9,8 +9,18 @@ import  User  from '@/interfaces/User';
 import {useRouter} from 'next/navigation'
 
 const CartPage = () => {
+  
+  
+  const router = useRouter()
+  const [popUpOpen,setpopUpOpen] = useState<boolean>(false)
+  const [checkoutLoder,setCheckoutLoder] = useState<boolean>(false)
+  const { cart, removeFromCart,addToCart,decrementItem,clearCart} = useContext(CartContext);
+ 
   const checkOut = async () => {
-    const getUser = localStorage.getItem('userId')!
+    try {
+     setCheckoutLoder(true)
+
+     const getUser = localStorage.getItem('userId')!
     const users:User[] = await client.fetch(`*[_type == 'users']`) 
     const userExists = users.find((e) => e._id === JSON.parse(getUser))
     if (userExists) {
@@ -41,13 +51,12 @@ const CartPage = () => {
     } else {
       setpopUpOpen(true)
     }
+   } catch (error) {
+    console.log("error checkout", error);
+    } finally {
+      setCheckoutLoder(false)
+   }
   }
-
-
-  const router = useRouter()
-  const [popUpOpen,setpopUpOpen] = useState<boolean>(false)
-  // const [checkout,setCheckout] = useState<boolean>(false)
-  const { cart, removeFromCart,addToCart,decrementItem,clearCart} = useContext(CartContext);
   return (
     <section className="text-gray-600 body-font overflow-hidden">
       <div className="container px-5 py-24 mx-auto">
@@ -132,9 +141,22 @@ const CartPage = () => {
                   <span>Total</span>
                   <span>${cart.reduce((total: number, item) => (total + parseInt((item.price - (item.price*(item.discountPercentage/100))).toFixed(0)) * item.minimumOrderQuantity) , 0) + 5}</span>
                 </div>
-                <button onClick={checkOut} className="bg-indigo-500 text-white py-2 px-4 w-full mt-5 rounded hover:bg-indigo-600">
+
+                {
+                  checkoutLoder ? 
+                  ( (<button disabled type="button" className="text-white bg-indigo-500 focus:ring-4 focus:ring-blue-300 py-2 px-4 w-full mt-5 rounded">
+                  <svg aria-hidden="true" role="status" className="inline w-4 h-4 me-3 text-white animate-spin" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="#E5E7EB"/>
+                  <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentColor"/>
+                  </svg>
+                  Loading...
+                  </button>
+                  ))
+                  
+                  :(<button onClick={checkOut} className="bg-indigo-500 text-white py-2 px-4 w-full mt-5 rounded hover:bg-indigo-600">
                   Proceed to Checkout
-                </button>
+                  </button>)
+                }
               </div>
             </div>
           </div>
