919d1474b83c0774bcc47f77da0f1a5a0fb31294
Muhammadtalha678 <muhammadtalha0110@gmail.com>
2025-01-15 20:08:30 +0500

    update sales object

--- src/app/api/sales/route.ts
+++ src/app/api/sales/route.ts
@@ -4,31 +4,31 @@ import { NextRequest, NextResponse } from "next/server";
 export async function POST(request: NextRequest) {
     const saleObj = await request.json()
     const sales = await client.create(
-                            {
-                              _type:'sales',
-                              ...saleObj
-                            }
-                            
-    )
-    const response = await fetch("https://api.goshippo.com/shipments/", {
-        method: "POST",
-        headers: {
-            Authorization: "Bearer TEST_TXBaPpehdbo3z06OuNjRdu34Ux8WApfJ5COyRwHPybg",
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-            address_from: "Saylani karachi",
-            address_to: sales.deliveryAddress.addressLine1,
-            parcels:sales.product_detail.length,
-            async: false,
-        },)
+        {
+            _type: 'sales',
+            ...saleObj
+        }
 
-    }) 
-    const shipment = await response.json();
-    console.log(shipment);
-    
+    )
+    // const response = await fetch("https://api.goshippo.com/shipments/", {
+    //     method: "POST",
+    //     headers: {
+    //         Authorization: "Bearer TEST_TXBaPpehdbo3z06OuNjRdu34Ux8WApfJ5COyRwHPybg",
+    //         "Content-Type": "application/json",
+    //     },
+    //     body: JSON.stringify({
+    //         address_from: "Saylani karachi",
+    //         address_to: sales.deliveryAddress.addressLine1,
+    //         parcels:sales.product_detail.length,
+    //         async: false,
+    //     },)
+
+    // }) 
+    // const shipment = await response.json();
+    // console.log(shipment);
+
     // const fetchSale = await client.fetch()
     return NextResponse.json({
-        error:false,data:sales
+        error: false, data: sales
     })
 }
